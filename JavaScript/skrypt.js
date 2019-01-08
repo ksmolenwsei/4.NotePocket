6ec0643acaea7edf32d14449038a3f28e7d5c16c
ksmolenwsei <krystian.smolen@microsoft.wsei.edu.pl>
2019-01-08 11:17:47 +0100

    wyświetlenie listy dodanych notatek

--- JavaScript/skrypt.js
+++ JavaScript/skrypt.js
@@ -1,13 +1,18 @@
 let addNoteButton = document.querySelector("#buttonAddNote")
 let popup = document.querySelector("#popUpWindowWithForm")
 let submitForm = document.querySelector("#submitButton")
+let tasksID = document.querySelector('#tasks')
+
+
 
 function getAndSetFormValues(){
-  
+    // let date = new Date()
+    // let fullDate = leadingZero(date.getHours()) + ':' + leadingZero(date.getMinutes()) + ':' + leadingZero(date.getSeconds() + " " + leadingZero(date.getDate()) + "." + leadingZero(date.getMonth()+1) + "." + date.getFullYear())
+    // console.log(fullDate)
     let data = {
     'title': document.querySelector("#title").value,
     'value': document.querySelector("#value").value,
-    'date': Date.now()
+    'date': new Date()
 
     }
     localStorage.setItem(localStorage.length + 1, JSON.stringify(data))
@@ -35,5 +40,23 @@ submitForm.addEventListener("click", function(){
 for(let i = 1; i<= localStorage.length; i++)
 {
     let retrievedObject = JSON.parse(localStorage.getItem(i))
-    console.log(retrievedObject)
+    console.log(retrievedObject.date)
+
+
+    let divForTask = document.createElement("div")
+    divForTask.classList.add("task")
+    tasksID.appendChild(divForTask)
+
+    let h2ForTitle = document.createElement("h2")
+    h2ForTitle.innerText = retrievedObject.title
+    divForTask.appendChild(h2ForTitle)
+
+    let placeForValue = document.createElement("h3")
+    placeForValue.innerText = retrievedObject.value
+    divForTask.appendChild(placeForValue)
+
+
+
+    
+
 }
